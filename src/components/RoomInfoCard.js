61de2151b7ba0edfbac56c71a13a9f38c771d969
Amritha-07 <p.p.amrithakumar@gmail.com>
2023-09-27 13:02:36 +0530

    ADD: Room Card

---
+++ src/components/RoomInfoCard.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react'
+import style from '../style/roomCard.module.css'
+import data from '../data/roomCard.json'
+import {BsChatDots, BsChatDotsFill, BsFillPersonFill } from 'react-icons/bs'
+
+export default function RoomInfoCard() {
+    return (
+        <>
+            <h2>
+                {data.map((item) => (
+                    <div>
+                        <div>
+                            <div className={style.RoomCardContainer}>
+                                <h6>{item.title}</h6>
+                                <h2>{item.sub_title}</h2>
+                                <div className={style.RoomMembers}>
+                                    <div>
+                                        <img src='/images/user-img.jpg' alt='' />
+                                        <img src='/images/user-img2.jpg' alt='' />
+                                    </div>
+                                    <div>
+                                        {item.members.map((person) => (
+                                            <p>
+                                                {person.first_name} {person.last_name} <BsChatDots />
+                                            </p>
+                                        ))}
+                                        <p className='d-flex align-items-center'>
+                                            <span className='me-2'>1.8</span>
+                                            <BsFillPersonFill />
+                                            <span className='mx-2'></span>{" "}
+                                            <span className='mr-2'>5</span> <BsChatDotsFill />
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </h2>
+        </>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Header from '../components/Header'
 import DailyInfoCard from '../components/DailyInfoCard'
 import style from '../style/home.module.css'
+import RoomInfoCard from '../components/RoomInfoCard'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { BsGrid3X3Gap } from 'react-icons/bs' 
 
 export default function Home() {
   return (
@@ -9,6 +12,16 @@ export default function Home() {
       <Header />
       <div className={style.HomeContainer}>
         <DailyInfoCard />
+        <RoomInfoCard />
+      </div>
+      <div className={style.ActionButton} >
+        <button>
+          <AiOutlinePlus className='me-2'/>
+          Start a room
+        </button>
+        <button>
+          <BsGrid3X3Gap />
+        </button>
       </div>
     </>
   )
